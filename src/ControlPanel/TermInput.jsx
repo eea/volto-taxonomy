f3542e04e48bea6d4f1d0e4ddcd095c4fa5104c3
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-07-17 20:29:05 +0200

    allow deleting items form a hierarchy

--- src/ControlPanel/TermInput.jsx
+++ src/ControlPanel/TermInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Grid, Button, Label, Input } from 'semantic-ui-react';
+import { Grid, Button, Label, Input, List } from 'semantic-ui-react';
 import { SEPARATOR } from '../constants';
 import { Icon } from '@plone/volto/components';
 import addSVG from '@plone/volto/icons/add.svg';
-import { v4 as uuid } from 'uuid';
+import deleteSVG from '@plone/volto/icons/delete.svg';
 
 const MultipleValuesTermInput = (props) => {
   const { entries, onChange } = props;
@@ -11,19 +11,38 @@ const MultipleValuesTermInput = (props) => {
 
   return (
     <>
-      {entries.map((item, index) => (
-        <Input
-          value={item}
-          key={index}
-          onChange={(ev, { value }) => {
-            let new_items = items;
-            new_items[index] = value;
-            setItems(new_items);
-            onChange('title', SEPARATOR + new_items.join(SEPARATOR));
-            onChange('hierarchy', new_items);
-          }}
-        />
-      ))}
+      <List divided relaxed>
+        {entries.map((item, index) => (
+          <List.Item>
+            <List.Content>
+              <Input
+                fluid
+                value={item}
+                key={index}
+                onChange={(ev, { value }) => {
+                  let new_items = items;
+                  new_items[index] = value;
+                  setItems(new_items);
+                  onChange('title', SEPARATOR + new_items.join(SEPARATOR));
+                  onChange('hierarchy', new_items);
+                }}
+              />
+              <Button
+                basic
+                onClick={() => {
+                  let new_items = items;
+                  new_items.splice(index, 1);
+                  setItems(new_items);
+                  onChange('title', SEPARATOR + new_items.join(SEPARATOR));
+                  onChange('hierarchy', new_items);
+                }}
+              >
+                <Icon className="circled" name={deleteSVG} size="12px" />
+              </Button>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
       <Button
         basic
         onClick={() => {
