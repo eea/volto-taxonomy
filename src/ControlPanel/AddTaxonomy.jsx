1be99a2d5c962987ebe79dbf011a63183ba208ce
Alin Voinea <contact@avoinea.com>
2026-04-08 13:10:16 +0300

    chore: Volto 18 eslint/jest migration - refs #301881 (#37)
    * chore: Upgrade eslint to Volto 18
    
    * chore: Volto 18 eslint/jest migration - refs #301881 (volto-taxonomy)

--- src/ControlPanel/AddTaxonomy.jsx
+++ src/ControlPanel/AddTaxonomy.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import { ModalForm, Toast } from '@plone/volto/components';
+import Toast from '@plone/volto/components/manage/Toast/Toast';
+import ModalForm from '@plone/volto/components/manage/Form/ModalForm';
 import { defineMessages, useIntl } from 'react-intl';
 import { getTaxonomySchema, addTaxonomy } from '../actions';
 
@@ -15,15 +16,14 @@ const messages = defineMessages({
     defaultMessage: 'Success',
   },
 });
+
 const AddTaxonomy = (props) => {
   const intl = useIntl();
   const { setShow } = props;
   const dispatch = useDispatch();
-  const [schema, loaded] = useSelector((state) => [
-    state.taxonomy?.schema?.schema,
-    state.taxonomy?.schema?.get?.loaded,
-  ]);
-  const [loading] = useSelector((state) => [state?.taxonomy?.add?.loading]);
+  const schema = useSelector((state) => state.taxonomy?.schema?.schema);
+  const loaded = useSelector((state) => state.taxonomy?.schema?.get?.loaded);
+  const loading = useSelector((state) => state?.taxonomy?.add?.loading);
 
   const [error, setError] = React.useState('');
 
